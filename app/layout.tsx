19fa610c3179d6003f794d1ad8955064dde93527
竹下駿 <takeshitashun@takeshitashunnoMacBook-Air.local>
2026-06-19 22:57:37 +0900

    Vercel Analyticsを導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+// Vercel Analyticsの部品をインポート
+import { Analytics } from "@vercel/analytics/react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "mine+ | 美祢市のイベント情報プラットフォーム",
-  description: "美祢市の現在をナビゲートするプラットフォーム。最新のイベント情報をリアルタイムに共有・発見できます。",
-  verification: {
-    // ここにいただいた確認コードをセットしました！
-    google: "QvHm5JWxLULRnRpU5CjHtaFiw0I__YUWklSsI5L50eU",
-  },
+  title: "mine+ | 美祢市のイベントプラットフォーム",
+  description: "美祢市の現在をナビゲートするプラットフォーム。最新のイベント情報をリアルタイムに共有。",
 };
 
 export default function RootLayout({
@@ -27,12 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="ja"
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        {children}
+        {/* アクセス数を計測するためのタグを配置（bodyの閉じタグの直前） */}
+        <Analytics />
+      </body>
     </html>
   );
 }
